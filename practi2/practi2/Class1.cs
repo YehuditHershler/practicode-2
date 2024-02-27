9d1a3490ac79c8919a28726b89515e5a6a47d7a0
YehuditHershler <149366076+YehuditHershler@users.noreply.github.com>
2024-02-27 22:52:59 +0200

    Update Class1.cs

--- practi2/practi2/Class1.cs
+++ practi2/practi2/Class1.cs
@@ -1,50 +1,3 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace practi2
-{
-    public static class Class1
-    {
-        static IEnumerable<HtmlElement> FindElements(this HtmlElement element, Selector selector)
-        {
-            var results = new HashSet<HtmlElement>();
-
-            if (Matches(element, selector))
-            {
-                results.Add(element);
-            }
-
-            if (selector.Child != null)
-            {
-                foreach (var child in element.Descendants())
-                {
-                    results.UnionWith((IEnumerable<HtmlElement>)child.FindElements(selector.Child));
-                }
-            }
-
-            return results;
-        }
-
-        private static bool Matches(HtmlElement element, Selector selector)
-        {
-            if (selector.TagName != null && element.Name != selector.TagName)
-                return false;
-
-            if (selector.Id != null && element.Id != selector.Id)
-                return false;
-
-            if (selector.Classes.Any() && !selector.Classes.All(c => element.Classes.Contains(c)))
-                return false;
-
-            return true;
-        }
-
-    }
-}*/
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,10 +9,8 @@ namespace practi2
         public static IEnumerable<HtmlElement> FindElements(this HtmlElement element, Selector selector)
         {
             if (Matches(element, selector))
-            {
                 yield return element;
-            }
-
+            
             if (selector.Child != null)
             {
                 foreach (var child in element.Descendants())
@@ -75,22 +26,12 @@ namespace practi2
         private static bool Matches(HtmlElement element, Selector selector)
         {
             if (selector.TagName != null && element.Name != selector.TagName)
-            {
                 return false;
-            }
-
             if (selector.Id != null && element.Id != selector.Id)
-            {
                 return false;
-            }
-
             if (selector.Classes.Any() && !selector.Classes.All(c => element.Classes.Contains(c)))
-            {
                 return false;
-            }
-
             return true;
         }
     }
 }
-
